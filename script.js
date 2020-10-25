8b6ec23a4ec5854d0af36b55d01fdd42df85f56f
Kirsti Schaffer <kirstischaffer@gmail.com>
2020-10-25 10:38:40 -0500

    astronaut count added

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 window.addEventListener("load", function() {
-    let json =[];
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         response.json().then(function(json) {
-            console.log(json);
+            // console.log(json);
+            const container = document.getElementById("container");
+            for(let i = 0; i < json.length; i++) {
+                // console.log(json[i]);
+                container.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                            <li>Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills.join(", ")}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${json[i].picture}">
+                </div>`;
+            }
+            //Add a count of astronauts to the page.
+            container.innerHTML +=
+            `<div class="astronautCount">
+                <p>Astronaut Count: ${json.length}</p>
+            </div>`
         });
     });
     
